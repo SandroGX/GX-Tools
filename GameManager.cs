a1e1a950f9752360337c1f575d4260c767a6dfb5
SandroGX <sandroalemflor@hotmail.com>
2019-09-21 14:33:59 +0100

    Updates to StateMachine System and created some new classes.
    This project will be divided into different ones after this commit. They'll be converted to Unity packages and be added back as submodules to this one.

---
+++ GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameObject PLAYER;
+        private static GameManager instance;
+        public static GameManager Instance
+        {
+            get
+            {
+                if (!instance)
+                {
+                    GameObject go = new GameObject("Game Manager");
+                    DontDestroyOnLoad(go);
+                    instance = go.AddComponent<GameManager>();
+                }
+                return instance;
+            }
+        }
+
+        private void Awake()
+        {
+            if (!instance)
+            {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else Destroy(gameObject);
+        }
+
+        public static Coroutine StartCoroutineGM(IEnumerator coroutine)
+        {
+            return Instance.StartCoroutine(coroutine);
+        }
+
+        public static void StopCoroutineGM(Coroutine coroutine)
+        {
+            Instance.StopCoroutine(coroutine);
+        }
+    }
+}
